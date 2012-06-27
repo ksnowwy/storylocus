7d28726514e6273e1ac0c9bef52a07f9dd2ae04f
ksnowwy <kelly.smith.holbourn@gmail.com>
2012-06-27 22:05:16 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storylocus::Application.config.secret_token = '7c0d88bf3e7399c96ad2000da645d6015585055642f67b92c23fa4efb79287a4eeeaeba883720756a5f2199857701314d0488fc7e6b1eaa1947cb9f87c8fa40d'
