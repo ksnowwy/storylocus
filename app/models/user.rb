9d375c9a02dfa8c8bf7f2cea2edfbeba1cc4deaf
ksnowwy <kelly.smith.holbourn@gmail.com>
2012-07-02 22:30:17 +1000

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
@@ -10,4 +11,10 @@ class User < ActiveRecord::Base
   validates :username,  presence: true, length: { maximum: 50 }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  private
+
+      def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Storylocus::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'static_pages#home'
   
   match '/signup', to: 'users#new'
-
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   match '/about',   to: 'static_pages#about'
 
 
